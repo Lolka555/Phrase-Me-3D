1c51ce02e9678fb606385e0adce5be82988eb1b1
Try_To_Take <70414732+Lolka555@users.noreply.github.com>
2023-02-25 23:41:21 +0300

    APP update

---
+++ PhraseMe3D/PhraseMe3D/Resources/PhraseMe3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhraseMe3DApp.swift
+//  PhraseMe3D
+//
+//  Created by Глеб Голощапов on 05.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhraseMe3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SignIn()
+        }
+    }
+}
