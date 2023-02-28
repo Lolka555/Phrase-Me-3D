b0b60d0425bd82719d73c144c592c463d1ce99d6
Try_To_Take <70414732+Lolka555@users.noreply.github.com>
2023-02-28 15:41:42 +0300

    app update

---
+++ PhraseMe3D/PhraseMe3D/ViewModel/SignUpViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  SingInViewModel.swift
+//  PhraseMe3D
+//
+//  Created by Глеб Голощапов on 15.02.2023.
+//
+
+import Foundation
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+//ViewModel для экрана с регистрацией
+class SignUpViewModel: ObservableObject{
+    
+    
+//    функция для отправки запроса на регистрацию
+    func SignUpAction(email: String, password: String, username: String, con: ((_ result: String, _ error: String) -> Void )? = nil) {
+        let url = "http://95.165.105.133:1221/register/user?username=\(username)&email=\(email)&password=\(password)"
+        AF.request(url, method: .get).validate().responseString() { response in
+            switch response.result{
+            case .success(let value):
+                let answer = value
+                con!(answer, "Succed")
+            case .failure(let error):
+                print(error.localizedDescription)
+                con!("", error.localizedDescription)
+            }
+        }
+    }
+    
+}
