55437105ef578b801a2bfc101a334c4936481c02
Mamlik <57287362+Mamlik@users.noreply.github.com>
2023-03-03 23:21:30 +0300

    PostRegistrationWithToken_AuthorizationWithToken

--- PM3D_API/main.py
+++ PM3D_API/main.py
@@ -7,18 +7,19 @@ from flask import Flask, abort, send_from_directory
 from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
 import json
+import datetime
 import jwt
+import uuid
 
 
-Port = 8080  # порт регистрации
-Server = 'localhost'  # api сервер
-
+Server = '192.168.1.3'  # api сервер
+Port = 1221  # порт регистрации
 
 db_session.global_init("db/data.db")  # подключение к базе данных
 
 users_blueprint = Blueprint(
     'hahaprof_app_api',
-    __name__
+    _name_
 )
 session = db_session.create_session()
 
@@ -58,7 +59,6 @@ def register_user():
         user_find_2 = session.query(User).filter(User.mail == email)
         if session.query(user_find.exists()).scalar() or session.query(user_find_2.exists()).scalar():
             return json.dumps({'status': 'fail', 'message': 'User or Email already registered'}), 416
-
         public_ide = str(uuid.uuid4())  # создание уникального id пользователя для создания jwt токенов при авторизации
         user = User(public_id=public_ide, name=username, mail=email, password=hash)
         session.add(user)
@@ -80,6 +80,7 @@ def login_user():
             return json.dumps({'status': 'fail', 'message': 'Incorrect Password '}), 412
         payload = {
             'user_id': user.public_id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60*60)
         }
         token = jwt.encode(payload, 'secret_key', algorithm='HS256')
         return json.dumps({'token': token}), 200
@@ -87,10 +88,22 @@ def login_user():
         return json.dumps({'status': 'fail', 'message': 'Authorization terminated due to unknown exception'}), 400
 
 
-app = Flask(__name__)  # создание flask приложения
+@users_blueprint.route('/username-by-token', methods=['POST'])  # возвращение username по токену
+def username_by_token():
+    try:
+        token = request.form['token']
+        data = jwt.decode(token, key='secret_key', algorithms=['HS256', ])
+        if session.query(User).filter(User.public_id == data["user_id"]).count() == 0:
+            return json.dumps({'status': 'fail', 'message': 'User not found'}), 404
+        user = session.query(User).filter(User.public_id == data["user_id"]).one()
+        return json.dumps({'email': user.mail}), 200
+    except Exception:
+        return json.dumps({'status': 'fail', 'message': 'Token not found'}), 400
+app = Flask(_name_)  # создание flask приложения
 app.config['SECRET_KEY'] = "secret_key"  # ключ для конфигурации
-app.config['models_dir'] = '/Users/egorurov/PycharmProjects/Phrase-Me-3D/PM3D_API/models'  # путь для хранения моделей
+app.config['models_dir'] = '/Users/egor/Desktop/backend-main/models'  # путь для хранения моделей
 
-if __name__ == '__main__':  # запуск api сервера
+
+if _name_ == '_main_':  # запуск api сервера
     app.register_blueprint(users_blueprint)
-    app.run(Server, Port)
+    app.run(Server, Port, debug=True)
